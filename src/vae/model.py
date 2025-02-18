c98422ee70deee6941b4f4abd0060e9fce804cb1
oliversparrowquantum <oliver.norregaard@sparrowquantum.com>
2025-02-18 16:16:13 +0000

    Update GAN discriminator to handle 128 x 128 images instead of 64 x 64

--- src/vae/model.py
+++ src/vae/model.py
@@ -276,21 +276,23 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.model = nn.Sequential(
              nn.ZeroPad2d(2),
-             nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1, bias=False),
+             nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1, bias=False), # 128 -> 64
              nn.LeakyReLU(0.2, inplace=True),
-             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, bias=False),
+             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, bias=False), # 64 -> 32
              nn.LeakyReLU(0.2, inplace=True),
-             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False), # 32 -> 16
              nn.LeakyReLU(0.2, inplace=True),
-             nn.Conv2d(128, 256, kernel_size=2, stride=2, padding=1, bias=False),
+             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False), # 16 -> 8
              nn.LeakyReLU(0.2, inplace=True),
-             nn.Conv2d(256, 1, kernel_size=2, stride=1, padding=0, bias=False),
+             nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False), # 8 -> 4
+             nn.LeakyReLU(0.2, inplace=True),
+             nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False), # 4 -> 1
          )
 
     def forward(self, x):
         x = x.to(torch.float32)
         # Remove singleton dimensions from output
-        return self.model(x).squeeze(3).squeeze(2).squeeze(1)
+        return self.model(x).squeeze()
 
 class GAN_Lightning(pl.LightningModule):
     def __init__(self, boson_sampler_params, gen_lr, disc_lr, latent_dim, output_size=64, output_dir="gan_images",
